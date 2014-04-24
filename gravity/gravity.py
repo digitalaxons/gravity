a0e6b176d5e6a93ddd9d183f02f9362857db2188
Derek <digitalaxons@gmail.com>
2014-04-25 00:59:30 +0400

    edits

--- gravity/gravity.py
+++ gravity/gravity.py
@@ -2,26 +2,11 @@ __author__ = 'Derek <digital axons (at) gmail>'
 import sys
 import os
 import syslog
+import gravity.config.msg
 
-def showbanner():
-	print
-	print ("Gravity rel 0.1 | April 2014 Concept")
-	print
 
-def showusage():
-	print("Usage: gravity [MODE][OPTIONS]...\n");
-	print("MODE");
-	print(" --on        Start deamon");
-	print(" --off       Switch off deamon");
-	print(" --restart   Restart deamon");
-	print(" --status    Brief status information\n");
-	print("OPTIONS");
-	print("")
-	print(" -c FILE     Configuraion file");
-	print(" -h          Display this help and exit");
-	print(" -v          Output version information and exit\n");
 
-showbanner()
-showusage()
+gravity.config.msg.showbanner()
+gravity.config.msg.showusage()
 syslog.openlog("gravity", logoption=syslog.LOG_PID)
 syslog.syslog("Gravity starting")
